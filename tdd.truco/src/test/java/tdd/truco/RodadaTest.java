b9774ac6c4cb52254f0b033d1217ac255c05b6d9
André Willik Valenti <awvalenti@gmail.com>
2013-11-27 23:42:33 -0200

    Melhorando teste com uso de Test Data Builder (ou quase isso, já que não há uma classe específica, apenas métodos estáticos)

--- tdd.truco/src/test/java/tdd/truco/RodadaTest.java
+++ tdd.truco/src/test/java/tdd/truco/RodadaTest.java
@@ -12,56 +12,60 @@ import org.junit.Test;
 public class RodadaTest {
 
 	private Rodada rodada;
-	private Ciclo ciclo1;
-	private Ciclo ciclo2;
-	private Ciclo ciclo3;
-	private Jogador jogador1;
-	private Jogador jogador2;
+	private Jogador jose;
+	private Jogador maria;
 
 	@Before
 	public void setUp() {
-		ciclo1 = mock(Ciclo.class);
-		ciclo2 = mock(Ciclo.class);
-		ciclo3 = mock(Ciclo.class);
-
-		jogador1 = new Jogador("Jogador 1");
-		jogador2 = new Jogador("Jogador 2");
+		jose = new Jogador("Jose");
+		maria = new Jogador("Maria");
 	}
 
 	@Test
-	public void deve_estar_em_andamento_quando_tiver_somente_um_ciclo() {
-		rodada = new Rodada(Arrays.asList(ciclo1));
+	public void deve_estar_em_andamento_quando_tiver_um_ciclo_em_andamento() {
+		rodada = rodadaCom(cicloEmAndamento());
+		assertThat(rodada.terminou(), is(false));
+	}
+
+	@Test
+	public void deve_estar_em_andamento_quando_tiver_um_ciclo_terminado() {
+		rodada = rodadaCom(cicloVencidoPor(jose));
 		assertThat(rodada.terminou(), is(false));
 	}
 
 	@Test
 	public void deve_estar_em_andamento_quando_dois_ciclos_terminarem_com_vencedores_diferentes() {
-		when(ciclo1.terminou()).thenReturn(true);
-		when(ciclo2.terminou()).thenReturn(true);
-		when(ciclo1.calcularVencedor()).thenReturn(jogador1);
-		when(ciclo2.calcularVencedor()).thenReturn(jogador2);
-
-		rodada = new Rodada(Arrays.asList(ciclo1, ciclo2));
+		rodada = rodadaCom(cicloVencidoPor(jose), cicloVencidoPor(maria));
 		assertThat(rodada.terminou(), is(false));
 	}
 
 	@Test
 	public void deve_terminar_quando_dois_ciclos_terminarem_com_mesmo_vencedor() {
-		when(ciclo1.calcularVencedor()).thenReturn(jogador1);
-		when(ciclo2.calcularVencedor()).thenReturn(jogador1);
-
-		rodada = new Rodada(Arrays.asList(ciclo1, ciclo2));
+		rodada = rodadaCom(cicloVencidoPor(jose), cicloVencidoPor(jose));
 		assertThat(rodada.terminou(), is(true));
 	}
 
 	@Test
 	public void deve_terminar_quando_tres_ciclos_terminarem() {
-		when(ciclo1.terminou()).thenReturn(true);
-		when(ciclo2.terminou()).thenReturn(true);
-		when(ciclo3.terminou()).thenReturn(true);
-
-		rodada = new Rodada(Arrays.asList(ciclo1, ciclo2, ciclo3));
+		rodada = rodadaCom(cicloVencidoPor(jose), cicloVencidoPor(jose), cicloVencidoPor(maria));
 		assertThat(rodada.terminou(), is(true));
+	}
+
+	private static Rodada rodadaCom(Ciclo... ciclos) {
+		return new Rodada(Arrays.asList(ciclos));
+	}
+
+	private static Ciclo cicloEmAndamento() {
+		Ciclo ciclo = mock(Ciclo.class);
+		when(ciclo.terminou()).thenReturn(false);
+		return ciclo;
+	}
+
+	private static Ciclo cicloVencidoPor(Jogador jogador) {
+		Ciclo ciclo = mock(Ciclo.class);
+		when(ciclo.terminou()).thenReturn(true);
+		when(ciclo.calcularVencedor()).thenReturn(jogador);
+		return ciclo;
 	}
 
 }
