794a002883b31b37c8e1b4759ff8f0dd828b8d45
André Willik Valenti <awvalenti@gmail.com>
2013-11-28 02:53:18 -0200

    Todos os testes passando, graças aos equals e hashCode do Eclipse

--- tdd.reservasala/src/main/java/tdd/reservasala/domain/Sala.java
+++ tdd.reservasala/src/main/java/tdd/reservasala/domain/Sala.java
@@ -27,13 +27,34 @@ public class Sala {
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		return obj instanceof Sala && id == ((Sala) obj).id;
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
 	}
 
 	@Override
-	public int hashCode() {
-		return id == null ? 0 : id.intValue();
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sala other = (Sala) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
 	}
 
 }
--- tdd.reservasala/src/main/java/tdd/reservasala/domain/Usuario.java
+++ tdd.reservasala/src/main/java/tdd/reservasala/domain/Usuario.java
@@ -34,13 +34,28 @@ public class Usuario {
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		return obj instanceof Usuario && id == ((Usuario) obj).id;
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
-	public int hashCode() {
-		return id == null ? 0 : id.intValue();
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 	@Override
